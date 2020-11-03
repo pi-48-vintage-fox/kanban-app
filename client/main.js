d87191e17ec056f014cb10ba882218bf152a9ff7
musabagja <bagja7musa@gmail.com>
2020-11-03 21:36:41 +0700

    Kanban Design Trial

--- client/main.js
+++ client/main.js
@@ -1,1 +1,39 @@
-$('#test').hide();
+const SERVER = 'http://localhost:3000'
+
+ready();
+
+function login(e) {
+  e.preventDefault();
+  const email = $('#login-email').val();
+  const password = $('#login-password').val();
+  $.ajax({
+    method: 'POST',
+    url: SERVER + '/login',
+    data: {
+      email, password
+    }
+  }).done(response => {
+    localStorage.setItem("token", response.access_token)
+    ready()
+  }).fail(err => {
+    Swal.fire(err.responseJSON.message)
+  })
+}
+
+function ready() {
+  $(document).ready(() => {
+    const token = localStorage.getItem("token")
+    if(token) {
+      $('#home-page').show();
+      $('#landing-page').hide();
+    } else {
+      $('#home-page').hide();
+      $('#landing-page').show();
+    }
+  })
+}
+
+function logout() {
+  localStorage.clear();
+  ready();
+}
