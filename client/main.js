6086d14f77a65c439c91666da5ca8aeb6dd7bf5f
khasogi27 <khasogi27@live.com>
2020-11-04 18:05:57 +0700

    1. server (baru init) 2. client (baru kanban board)

---
+++ client/main.js
@@ -0,0 +1,10 @@
+// Mobile Navigation Bar
+const myFunction = () => {
+  let x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
