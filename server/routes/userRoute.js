4f0d5ea26f444fa0d72f73245a418ade557b3772
Mochaa <mochamadzulfikar@gmail.com>
2020-11-05 20:46:33 +0700

    add assign user to organization route ("/assign")

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,7 +1,8 @@
 'use strict'
 const express = require('express');
+const UserController = require('../controllers/UserController');
 const userRoute = express.Router()
 
-
+userRoute.post("/assign",UserController.assign)
 
 module.exports = userRoute
