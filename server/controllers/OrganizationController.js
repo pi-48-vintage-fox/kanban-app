ad3d1e6bcec9326f51c9bf2a46a6dce9ed0521e8
Mochaa <mochamadzulfikar@gmail.com>
2020-11-05 15:53:34 +0700

    Add OrganizationController - get all organization function (OrganizationController) - insert new organization function (OrganizationController) - update organization function (OrganizationController) - delete organization function

--- server/controllers/OrganizationController.js
+++ server/controllers/OrganizationController.js
@@ -1,6 +1,71 @@
 'use strict'
+const { Organization } = require("../models")
+class OrganizationController {
 
-class OrganizationController{
+  static async index(req, res, next) {
+    try {
+      let organizations = Organization.findAll()
+      res.status(200).json({
+        msg: "Success",
+        organizations
+      })
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  static async insert(req, res, next) {
+    try {
+      let data = {
+        name: req.body.name
+      }
+
+      let organization = await Organization.create(data)
+
+      res.status(200).json({
+        msg: "Organization Added",
+        organization
+      })
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  static async update(req, res, next) {
+    try {
+      let id = req.params.id
+      let data = {
+        name: req.body.name
+      }
+
+      let organization = await Organization.update(data,{
+        where:{
+          id
+        }
+      })
+      
+      res.status(200).json({
+        msg : "Organization updated",
+        organization
+      })
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  static async destroy(req,res,next){
+    try {
+      let id = req.params.id
+      let organization = await Organization.findByPk(id)
+      res.status(200).json({
+        msg : "Organization Deleted",
+        organization
+      })
+      organization.destroy()
+    } catch (err) {
+      next(err)
+    }
+  }
 
 }
 
