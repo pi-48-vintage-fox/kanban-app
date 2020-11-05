d2a372915badc00f9868deaf50f1f720dc3ddb90
Mochaa <mochamadzulfikar@gmail.com>
2020-11-05 20:52:14 +0700

    add task Routes

--- server/routes/tasksRoute.js
+++ server/routes/tasksRoute.js
@@ -1,5 +1,12 @@
 'use strict'
 const express = require('express');
+const TaskController = require('../controllers/TaskController');
 const taskRoute = express.Router()
+
+taskRoute.get('/',TaskController.index)
+taskRoute.post('/',TaskController.insert)
+taskRoute.put("/:id",TaskController.update)
+taskRoute.patch("/:id",TaskController.updateTaskCategory)
+taskRoute.delete("/:id",TaskController.destroy)
 
 module.exports = taskRoute
