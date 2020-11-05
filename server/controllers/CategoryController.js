1368cb13b1c502ee43ec35218a667f4e1dc44061
Mochaa <mochamadzulfikar@gmail.com>
2020-11-05 20:34:26 +0700

    CategoryController - add get all categories function (CategoryController) - add create category function (CategoryController) - add edit category function (CategoryController) - add delete category function (CategoryController)

---
+++ server/controllers/CategoryController.js
@@ -0,0 +1,76 @@
+'use strict'
+
+const { Category } = require("../models")
+
+class CategoryController {
+
+  static async index(req, res, next) {
+    try {
+      let categories = await Category.findAll()
+
+      res.status(200).json({
+        msg: 'Success',
+        categories
+      })
+
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  static async insert(req, res, next) {
+    try {
+      let data = {
+        name: req.body.name
+      }
+
+      let category = await Category.create(data)
+      res.status(200).json({
+        msg: "Add category success",
+        category
+      })
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  static async update(req, res, next) {
+    try {
+      let id = req.params.id
+      let data = {
+        name: req.body.name
+      }
+
+      let category = await Category.update(data, {
+        where: {
+          id
+        }
+      })
+
+      res.status(200).json({
+        msg: "Category edited successfully",
+        category
+      })
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  static async delete(req, res, next) {
+    try {
+      let id = req.params.id
+
+      let category = await Category.findByPk(id)
+      req.status(200).json({
+        msg: "Category deleted",
+        category
+      })
+      category.destroy()
+    } catch (err) {
+      next(err)
+    }
+  }
+
+}
+
+module.exports = CategoryController
