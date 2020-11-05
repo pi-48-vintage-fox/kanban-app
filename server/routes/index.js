08665de208cbd4b553906c8307eb794a79db4bbc
Mochaa <mochamadzulfikar@gmail.com>
2020-11-05 20:54:54 +0700

    add authenticate

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,11 +5,13 @@ const userRoute = require('./userRoute');
 const organizationRoute = require('./organizationRoute');
 const UserController = require('../controllers/UserController');
 const categoryRoutes = require('./categoryRoutes');
+const authenticate = require('../middlewares/authMiddleware');
 const Routes = express.Router()
 
 Routes.post("/login",UserController)
 Routes.post("/register",UserController)
 
+Routes.use(authenticate)
 Routes.use("/tasks",taskRoute)
 Routes.use("/users",userRoute)
 Routes.use("/organizations",organizationRoute)
