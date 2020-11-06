2ba2d6fe944c0860a661bb3eb2c3955fabfb6560
daryalfuad <daryalfuad@gmail.com>
2020-11-07 00:30:12 +0700

    salah dikit

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://kanbandaryal.herokuapp.com',
     // timeout: 1000,
     // headers: {'X-Custom-Header': 'foobar'}
   });
