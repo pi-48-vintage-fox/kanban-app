81dc5dc7caa522a171de645509137d21e3cc20c4
ReynaldiSeptian19 <reynaldiseptian019@gmail.com>
2020-11-07 05:21:47 +0700

    deploy client

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://kanban-app-septian.herokuapp.com/',
   });
 
 export default instance
