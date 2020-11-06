cf49e195c106e5e6b8e49f947aafaa0a7ad400cc
daryalfuad <daryalfuad@gmail.com>
2020-11-07 00:12:18 +0700

    salah dikit

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
+if(process.env.NODE_ENV != 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
 const errorHandler = require('./middlewares/errorHandler')
 const app = express()
 const port = 3000
 const routes = require('./routes/index')
 const cors = require('cors')
-if(process.env.NODE_ENV != 'production') {
-  require('dotenv').config()
-}
+
 
 app.use(cors())
 app.use(express.json())
