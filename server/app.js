0d47775b588d09690d20ef49247f76bcaaa8daa6
Bimo dwien Prabowo <bimodwienprabowo@Bimos-MacBook-Air.local>
2020-11-07 02:01:14 +0700

    update: error handler

--- server/app.js
+++ server/app.js
@@ -8,11 +8,13 @@ const app = express()
 const cors = require('cors')
 const routes = require('./routes/route')
 const port = process.env.PORT || 3000
+const errorHandler = require('./middlewares/auth')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use('/', routes)
+app.use(errorHandler)
 
 app.listen(port,()=>{
   console.log(`I love you ${port}`)
