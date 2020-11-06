bd9d3f638c8e64daa73902c5bb5fac9b7cd75341
Haris Pratama <hpratama324@gmail.com>
2020-11-07 00:15:43 +0700

    3

--- server/app.js
+++ server/app.js
@@ -1,4 +1,7 @@
-require('dotenv').config();
+if (proccess.env.NODE_ENV != 'production') {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
