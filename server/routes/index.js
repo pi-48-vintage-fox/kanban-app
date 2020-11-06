5eaf26f30ebfabba2273d6e149a3d6b9fa4d32f0
ReynaldiSeptian19 <reynaldiseptian019@gmail.com>
2020-11-06 21:07:23 +0700

    coba deploy

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,9 @@ const router = require('express').Router()
 const todoRouter = require('./task')
 const userRouter = require('./user')
 
-
+router.get('/', (req,res) =>{
+res.send('kanban App')
+})
 router.use('/task', todoRouter)
 router.use('/user', userRouter)
 
