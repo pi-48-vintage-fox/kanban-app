de8f95117acdccf239fbc47cc95bf70c32b21a44
rey-h8 <rey-h8@gmail.com>
2020-11-07 19:29:28 +0700

    change baseurl to heroku

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const cors = require('cors')
 const router = require('./routes')
 
 app.use(cors())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use(router)
