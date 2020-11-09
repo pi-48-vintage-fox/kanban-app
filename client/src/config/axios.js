fe649ba15903921adff8d0ed063bb43b0596c8da
brotheryeska <yeskahaganta3838@gmail.com>
2020-11-09 15:56:53 +0700

    all done

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const instance = axios.create({
     // baseURL: 'http://localhost:3000'
-    baseURL: 'http://kanban-app-yeska.herokuapp.com'
+    baseURL: 'https://kanban-app-yeska.herokuapp.com'
 })
 
 export default instance 
