f7e1711aefa19db547002a161d63de3caedc09c4
khasogi27 <khasogi27@live.com>
2020-11-19 19:26:02 +0700

    update deploy heroku

--- server/seeders/20201109094534-seeding-data-category.js
+++ server/seeders/20201109094534-seeding-data-category.js
@@ -2,15 +2,6 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
     const data = [
       {
         name: "Backlog",
@@ -35,12 +26,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
     return queryInterface.bulkDelete('Categories', null)
   }
 };
--- server/seeders/20201109100952-seeding-data-task.js
+++ server/seeders/20201109100952-seeding-data-task.js
@@ -1,16 +1,7 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: (queryInterface, Sequelize) => {
     const data = [
       {
         "title": "Live code 1",
@@ -47,13 +38,7 @@ module.exports = {
     return queryInterface.bulkInsert("Tasks", data)
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete("Tasks", null)
   }
 };
